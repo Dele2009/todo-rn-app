568b9538ced4c1d7a36ec19be20fd03d6700f00f
Bamidele Aminu <obedaminu303@gmail.com>
2024-08-08 14:44:36 +0100

    bottom tabs

--- App.js
+++ App.js
@@ -1,14 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+// Screens
+import Home from './screens/Home';
+import Menu from './screens/Menu';
+import { Feather } from '@expo/vector-icons';
+const Stack = createNativeStackNavigator()
+const Drawer = createDrawerNavigator()
+const Tab = createBottomTabNavigator()
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto"/>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{
+        tabBarActiveTintColor: 'green',
+        headerShadowVisible: false,
+        headerTintColor: '#ff0000',
+        headerStyle: styles.header,
+        contentStyle: styles.homeContent
+      }}>
+        <Tab.Screen
+          name='Home'
+          component={Home}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <Feather name='home' size={30} color={color} />
+            )
+          }}
+        />
+        <Tab.Screen
+          name='Menu'
+          component={Menu}
+          options={{
+            headerShadowVisible: false,
+            tabBarIcon: ({color, size}) => (
+              <Feather name='menu' size={30} color={color} />
+            )
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+
   );
 }
 
@@ -19,4 +55,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'green',
+    // marginTop: 50
+  },
+  homeContent: {
+    backgroundColor: 'red'
+  }
 });
