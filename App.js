a4df58657aa808763a16dc5328d13073872a14a6
Bamidele Aminu <obedaminu303@gmail.com>
2024-08-10 14:29:35 +0100

    new update

--- App.js
+++ App.js
@@ -1,55 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Image, FlatList, Text, TouchableOpacity, View, TextInput, Button } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
-import Modal from 'react-native-modal';
-import Header from './components/header';
-import TaskItem from './components/TaskItem';
-import { useState } from 'react';
 
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { NavigationContainer } from '@react-navigation/native'
+import Home from './screens/Home';
 // import DefaultImage from ;
 
-const RootNav = () => {
-  return (
-    <Drawer.Navigator>
-      <Drawer.Screen
-        name='Home'
-        component={Home}
-      />
-    </Drawer.Navigator>
-  )
-}
-
+// const RootNav = () => {
+//   return (
+//     <Drawer.Navigator>
+//       <Drawer.Screen
+//         name='Home'
+//         component={Home}
+//       />
+//     </Drawer.Navigator>
+//   )
+// }
+const Stack = createNativeStackNavigator()
 export default function App() {
 
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{
-        tabBarActiveTintColor: 'green',
-        headerShadowVisible: false,
-        headerTintColor: '#ff0000',
-        headerStyle: styles.header,
-        contentStyle: styles.homeContent
-      }}>
-        <Tab.Screen
-          name='RootNav1'
-          component={RootNav}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Feather name='home' size={30} color={color} />
-            )
-          }}
+      <Stack.Navigator
+      screenOptions={{
+        headerShown: false
+      }}
+      >
+        <Stack.Screen
+          name='Home'
+          component={Home}
         />
-        <Tab.Screen
-          name='Menu'
-          component={Menu}
-          options={{
-            headerShadowVisible: false,
-            tabBarIcon: ({ color, size }) => (
-              <Feather name='menu' size={30} color={color} />
-            )
-          }}
-        />
-      </Tab.Navigator>
+      </Stack.Navigator>
     </NavigationContainer>
 
   );
--- screens/Home.js
+++ screens/Home.js
@@ -1,4 +1,4 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 import PrimaryButton from '../components/PrimaryButton'
@@ -8,16 +8,73 @@ import PrimaryButton from '../components/PrimaryButton'
 const Home = () => {
     const navigation = useNavigation()
     return (
-        <View>
-            <Text>Home</Text>
-            <PrimaryButton onPress={() => navigation.navigate('Menu')} >
+        <View style={styles.homeContainer}>
+            <View style={styles.heroContainer}>
+                <Image style={styles.heroImage}
+                    source={require('../assets/img/hero-img.png')}
+                />
+            </View>
+
+            <View style={styles.textContainer}>
+                <Image style={styles.textLogo}
+                    source={require('../assets/img/hero-img.png')}
+                />
+
+                <Text style={styles.text}>Localshop</Text>
+            </View>
+            {/* <PrimaryButton
+                propStyles={buttonStyles}
+                onPress={() => navigation.navigate('Menu')}
+            >
                 Go to menu
-            </PrimaryButton>
-            
+            </PrimaryButton> */}
+
         </View>
     )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const buttonStyles = StyleSheet.create({
+    buttonInnerContainer: {
+        backgroundColor: 'blue',
+        paddingVertical: 16,
+        paddingHorizontal: 16,
+        elevation: 2,
+    },
+    buttonText: {
+        color: "white",
+        textAlign: "center",
+    },
+    pressed: {
+        opacity: 0.75,
+    },
+})
+const styles = StyleSheet.create({
+    homeContainer: {
+        paddingHorizontal: 20
+    },
+    heroContainer: {
+        width: '100%',
+        height: 500,
+    },
+    heroImage: {
+        width: '100%',
+        height: '100%',
+    },
+    textContainer: {
+        flexDirection: 'row',
+        // justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%'
+    },
+    textLogo: {
+        width: 60,
+        height: 60
+    },
+    text: {
+        fontWeight: 'bold',
+        fontSize: 20
+    }
+
+})
